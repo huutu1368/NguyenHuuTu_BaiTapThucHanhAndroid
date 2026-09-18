f4f726269bb968bb95bf6b7024dc5553d37067ce
Nguyen Huu Tu <nguyenhuutu29042006@gmail.com>
2026-09-18 09:17:16 +0700

    Thiết lập MainActivity.kt va hoan thien bai tap thuc hanh

--- app/src/main/java/com/example/nguyenhuutu/MainActivity.kt
+++ app/src/main/java/com/example/nguyenhuutu/MainActivity.kt
@@ -1,14 +1,17 @@
 package com.example.nguyenhuutu
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.example.nguyenhuutu.databinding.ActivityMainBinding
 import com.example.nguyenhuutu.model.Student
+import com.example.nguyenhuutu.utils.showConfirmDialog
 import com.example.nguyenhuutu.utils.toAcademicRanking
 import com.example.nguyenhuutu.utils.toast
 
 class MainActivity : AppCompatActivity() {
-
     private lateinit var binding: ActivityMainBinding
 
     private var currentStudent = Student(
@@ -19,56 +22,52 @@ class MainActivity : AppCompatActivity() {
         phone = "0779116679",
         gpa = 3.8
     )
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
-
         setContentView(binding.root)
         bindStudentData(currentStudent)
-
         binding.btnUpdateGpa.setOnClickListener {
-
-            val inputStr =
-                binding.edtNewGpa.text.toString().trim()
-            val newGpa =
-                inputStr.toDoubleOrNull()
+            val inputStr = binding.edtNewGpa.text.toString().trim()
+            val newGpa = inputStr.toDoubleOrNull()
             if (newGpa == null || newGpa !in 0.0..4.0) {
-
                 binding.edtNewGpa.error =
                     "Vui lòng nhập GPA hợp lệ (0.0 - 4.0)"
-
-                toast("Điểm GPA không hợp lệ!")
-
+                toast(
+                    "Điểm GPA không hợp lệ!"
+                )
                 return@setOnClickListener
             }
-            currentStudent =
-                currentStudent.copy(
-                    gpa = newGpa
-                )
+            currentStudent = currentStudent.copy(gpa = newGpa)
             bindStudentData(currentStudent)
             toast("Cập nhật điểm thành công!")
         }
+
+        binding.btnCall.setOnClickListener {
+            val phoneNumber = currentStudent.phone
+            val intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:$phoneNumber"))
+            startActivity(intent)
+        }
+
+        binding.btnDelete.setOnClickListener {
+
+            showConfirmDialog(
+                title = "Xác nhận xóa",
+                message = "Bạn có chắc chắn muốn xóa hồ sơ sinh viên này không?") {
+                binding.cardProfile.visibility = View.GONE
+                toast("Đã xóa hồ sơ!")
+            }
+        }
     }
-
-
     private fun bindStudentData(student: Student) {
 
         with(binding) {
-
-            tvName.text =
-                student.name
-
-            tvStudentId.text =
-                "MSSV: ${student.id} • Lớp: ${student.className}"
-
-            tvGpaBadge.text =
-                "${student.gpa} GPA (${student.gpa.toAcademicRanking()})"
-
-            edtNewGpa.setText(
-                student.gpa.toString()
-            )
+            tvName.text = student.name
+            tvStudentId.text = "MSSV: ${student.id} • Lớp: ${student.className}"
+            tvPhone.text = "SĐT: ${student.phone}"
+            tvGpaBadge.text = "${student.gpa} GPA (${student.gpa.toAcademicRanking()})"
+            edtNewGpa.setText(student.gpa.toString())
         }
     }
 }
